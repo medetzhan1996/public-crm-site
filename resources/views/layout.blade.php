573378625adc967a5b75621ace061eef2aaac2a5
medetzhan1996 <67154220+medetzhan1996@users.noreply.github.com>
2020-06-29 21:37:35 +0600

    font-awesome добавил Елдос

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,11 +11,13 @@
 
 	<!-- Google Font -->
 	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
+	<link href="{{ asset('fonts/Krona_One/.min.css') }}" rel="stylesheet" type="text/css" />
 
 
 	<!-- Stylesheets -->
 	<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"/>
 	<link rel="stylesheet" href="{{ asset('css/fontawesome/css/font-awesome.min.css') }}"/>
+	<link rel="stylesheet" href="{{ asset('css/fonts.css') }}"/>
 	<link rel="stylesheet" href="{{ asset('css/css/flaticon.css') }}"/>
 	<link rel="stylesheet" href="{{ asset('css/slicknav.min.css') }}"/>
 	<link rel="stylesheet" href="{{ asset('css/jquery-ui.min.css') }}"/>
